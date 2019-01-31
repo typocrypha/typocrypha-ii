2ef238dcb11f36a9e4f9a6123bc90d6f0745000b
James Takeshi Iwamasa <jiwamasa@ucsc.edu>
2019-01-30 18:00:22 -0800

    Initial Project Commit

---
+++ Typocrypha II/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System.IO;
+
+/// <summary>
+/// Management of audio assets by filename.
+/// Not for playback use.
+/// </summary>
+/// <example>
+/// <c>AudioManager.instance["clip name"]</c>
+/// </example>
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance = null; // Global static instance
+
+    AssetBundle audioBundle; // Asset bundle containing all clips
+    Dictionary<string, AudioClip> clips; // Maps filename to loaded Audio Clips
+
+    public AudioClip this[string clipName] // Allows access to audio clips by name
+    {
+        get
+        {
+            Load(clipName);
+            return clips[clipName];
+        }
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } 
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+
+        audioBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "audio"));
+        if (audioBundle == null)
+        {
+            Debug.Log("Failed to load Audio");
+            return;
+        }
+
+        clips = new Dictionary<string, AudioClip>();
+    }
+
+    /// <summary>
+    /// Load clip by name if not already loaded.
+    /// </summary>
+    public void Load(string clipName)
+    {
+        if (!clips.ContainsKey(clipName))
+        {
+            if (audioBundle.Contains(clipName))
+            {
+                clips.Add(clipName, audioBundle.LoadAsset<AudioClip>(clipName));
+            }
+            else
+            {
+                throw new System.Exception("Trying to load audio asset that doesnt exist:" + clipName);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Load all AudioClips.
+    /// </summary>
+    public void LoadAll()
+    {
+        AudioClip[] allClips = audioBundle.LoadAllAssets<AudioClip>();
+        foreach (AudioClip clip in allClips)
+        {
+            if (!clips.ContainsKey(clip.name))
+            {
+                clips.Add(clip.name, clip);
+            }
+        }
+    }
+}
---
+++ Typocrypha II/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manages camera effects.
+/// </summary>
+public class CameraManager : MonoBehaviour
+{
+    public static CameraManager instance = null; // Global static instance
+    public Transform cameraTr; // Transform of the camera
+    public GameObject faderPrefab; // Prefab of fader plane
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    /// <summary>
+    /// Shakes camera.
+    /// </summary>
+    /// <param name="intensity">Amount of shaking.</param>
+    /// <param name="length">Length of shaking in seconds.</param>
+    public void Shake(float intensity, float length)
+    {
+        StartCoroutine(_Shake(intensity, length));
+    }
+
+    // Coroutine for shaking
+    IEnumerator _Shake(float intensity, float length)
+    {
+        float endTime = Time.time + length;
+        Vector3 oldPos = cameraTr.position;
+        while (Time.time < endTime)
+        {
+            cameraTr.position = oldPos + (Vector3)Random.insideUnitCircle;
+            yield return null;
+        }
+        cameraTr.position = oldPos;
+    }
+}
