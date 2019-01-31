db71ad2fcbbb260d06a6ba8dba9c8b6033ed7963
James Takeshi Iwamasa <jiwamasa@ucsc.edu>
2019-01-31 11:33:06 -0800

    Ported over more scripts
    Mostly basic graphics processing and utility classes.

--- Typocrypha II/Assets/OLD/GameflowSystem/DialogSystem/Dialog/DialogItems/DialogItemAN.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DialogItemAN : DialogItem { public DialogItemAN(string text) : base(text) { } };
---
+++ Typocrypha II/Assets/Scripts/Graphics/PostProcessing/PostProcess.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Generic post processing effect on camera.
+/// </summary>
+public class PostProcess : MonoBehaviour
+{
+	public static List<PostProcess> allPostProcesses = null; // List of all post process effects
+	public Material mat; // Material for shader effect
+    public Dictionary<string, float> shaderParams; // Shader property parameters (mapped by name)
+
+	void Awake()
+    {
+        if (allPostProcesses == null)
+        {
+            allPostProcesses = new List<PostProcess>();
+        }
+		allPostProcesses.Add (this);
+        shaderParams = new Dictionary<string, float>();
+	}
+
+	void Update()
+    {
+        foreach (var kvp in shaderParams)
+        {
+            mat.SetFloat(kvp.Key, kvp.Value);
+        }
+	}
+
+	void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+		Graphics.Blit (src, dest, mat);
+	}
+}
