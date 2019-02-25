eac3b9e3c6a14c1c98e8af22c44a950c00396d35
James Takeshi Iwamasa <jiwamasa@ucsc.edu>
2019-02-24 22:58:49 -0800

    Reorganized dialog system files

---
+++ Typocrypha II/Assets/Scripts/DialogSystem/UI/DialogSpacebar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manages behavior of spacebar icon during dialog.
+/// </summary>
+[RequireComponent(typeof(SpriteRenderer), typeof(Animator))]
+public class DialogSpacebar : MonoBehaviour
+{
+    SpriteRenderer sr;
+    Animator animator;
+
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Check state of dialog, and update visuals appropriately.
+    void Update()
+    {
+
+    }
+}
