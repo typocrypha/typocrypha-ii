fbfca16da85fd7327e0d3a1bfa9d3ebcb5920e5c
Valentino Abate <valentinoabate1@gmail.com>
2019-02-28 15:32:09 -0800

    Spell/casting top-level design and pseudocode
    -Spells can be constructed (by the their constructor) from lists of keyword objects and then cast by calling the Cast method

---
+++ Typocrypha II/Assets/Scripts/BattleSystem/Casting/Spell.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell
+{
+    // Spell tag dictionary
+    // Accessible spelleffects (already modified or list of modifications)
+    
+    // Constructor(list of spell word object)
+        //Search for modifiers. apply modifiers
+
+    public void Cast (Caster caster, Caster target)
+    {
+        //Foreach root?
+        //Foreach spell effect
+            //Target the effect (return list of positions)
+            //Foreach target position
+                //if no target
+                    //no target visualization
+                //else
+                    //Apply effect to target (log effect data)
+                    //Spawn visualization
+                    //Break if enemy killed?
+        //Do total visualization?
+        //When do enemies die?
+    }
+}
