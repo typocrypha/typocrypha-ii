5489dbc30ec1ff05b7adb560799abdcecbdcc81d
James Takeshi Iwamasa <jiwamasa@ucsc.edu>
2019-03-10 15:34:06 -0700

    Put Jiggle into appropriate namespace

--- Typocrypha II/Assets/Scripts/Utility/AdhocAnimations/Jiggle.cs
+++ Typocrypha II/Assets/Scripts/Utility/AdhocAnimations/Jiggle.cs
@@ -2,27 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// Jiggles attached object in place.
-/// </summary>
-public class Jiggle : MonoBehaviour
+namespace AdhocAnim
 {
-    public float intensity = 0.2f;
-    public float delay = 0.02f;
-    Vector3 basePos;
+    /// <summary>
+    /// Jiggles attached object in place.
+    /// </summary>
+    public class Jiggle : MonoBehaviour
+    {
+        public float intensity = 0.2f;
+        public float delay = 0.02f;
+        Vector3 basePos;
 
-    void Start()
-    {
-        basePos = transform.position;
-        StartCoroutine(JiggleCR());
-    }
-    
-    IEnumerator JiggleCR()
-    {
-        for (; ; )
+        void Start()
         {
-            transform.position = basePos + Random.insideUnitSphere * intensity;
-            yield return new WaitForSeconds(delay);
+            basePos = transform.position;
+            StartCoroutine(JiggleCR());
+        }
+
+        IEnumerator JiggleCR()
+        {
+            for (; ; )
+            {
+                transform.position = basePos + Random.insideUnitSphere * intensity;
+                yield return new WaitForSeconds(delay);
+            }
         }
     }
 }
+
