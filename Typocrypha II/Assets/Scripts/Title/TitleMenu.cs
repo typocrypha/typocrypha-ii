f476dde89a6c82ca0b3f541bf4d10ea3d6d9d7e9
Justin Law <jlawhome08@gmail.com>
2019-03-27 20:02:11 -0700

    Created Title Scene
    Effectively replicated the original title screen (not the entire sequence, just the main menu) + animated background for fun.

---
+++ Typocrypha II/Assets/Scripts/Title/TitleMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TitleMenu : MonoBehaviour
+{
+    public bool canContinue = true;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (canContinue) {
+            GameObject resumeButton = transform.GetChild(0).gameObject;
+            resumeButton.SetActive(true);
+            EventSystem.current.firstSelectedGameObject = resumeButton;
+            transform.GetChild(1).gameObject.SetActive(true);
+            
+        }
+    }
+}
