fed86ef576bc6d669b99203c8c89ec71bb433d2c
James Takeshi Iwamasa <jiwamasa@ucsc.edu>
2019-05-17 17:50:05 -0700

    Added burning key effect

---
+++ Typocrypha II/Assets/Scripts/Keyboard/Effects/KeyEffectBurning.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Typocrypha
+{
+    /// <summary>
+    /// Burning key effect.
+    /// Affected key's output is doubled.
+    /// </summary>
+    public class KeyEffectBurning : KeyEffect
+    {
+        const float time = 5f; // Duration of fire.
+
+        public override void OnStart()
+        {
+            // Double output.
+            key.output = key.output + key.output;
+            // Start timer.
+            StartCoroutine(DestroyAfterTime(time));
+        }
+
+        public override void OnPress()
+        {
+            
+        }
+
+        public override void Reset()
+        {
+            // Revert output.
+            key.output = key.letter.ToString();
+            key.onPress -= OnPress;
+        }
+
+        // Remove effect when time runs out.
+        IEnumerator DestroyAfterTime(float seconds)
+        {
+            yield return new WaitForSecondsPause(seconds, PH);
+            Remove();
+        }
+    }
+}
+
