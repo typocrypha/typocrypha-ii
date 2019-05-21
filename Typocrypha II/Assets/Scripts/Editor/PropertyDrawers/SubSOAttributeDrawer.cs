3dbfeb6c731f2a5bde341423505cf5bb9b492117
Valentino Abate <valentinoabate1@gmail.com>
2019-05-21 16:29:50 -0700

    SubSO property attribute
    -You can now add the SubSO attribute to any spriptable object field in a scriptable object and it will automatically draw it as a SubSO
    -SubSO allows you to create a new instance as a sub-asset (with deletion) and show the sub-assets inpector in an AuxWindow With a button
    -ITS CLEAN AND POLYMORPHINC AND A PROPERTY ATTRIBUTE I DID IT ITS BEAUTIFUL
    -This should allow me to know out the eidtor stuff for caster abilities, custom damage formulae, custom modifier effects... EVERYTHING
    -Creted the "InspectorEditorWindow" Class to support this (just shows an objects inspector in a separate window)
    -Expanded relective enumertaor to be able to take a System.Type arg in addition to a generic method
    -TODO: add some supprt for nesting in the menu
    -TODO: test with property field in custom inspector
    -TODO: make sure the attribute isn't used on other things

---
+++ Typocrypha II/Assets/Scripts/Editor/PropertyDrawers/SubSOAttributeDrawer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(SubSOAttribute))]
+public class SubSOAttributeDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, GUIContent.none, property);
+        var attr = attribute as SubSOAttribute;
+        var UIRect = new Rect(position) { height = EditorGUIUtility.singleLineHeight };
+        var dropRect = new Rect(UIRect) { width = UIRect.width - 125 };
+        string dropString = "Set " + attr.name + (property.objectReferenceValue == null ? " (None)" : property.objectReferenceValue.ToString());
+        if (EditorGUI.DropdownButton(dropRect, new GUIContent(dropString), FocusType.Keyboard))
+        {
+            GetMenu(property, fieldInfo.FieldType).ShowAsContext();
+        }
+        var inspectButtonRect = new Rect(UIRect) { x = UIRect.x + dropRect.width, width = 125};
+        if (property.objectReferenceValue != null && GUI.Button(inspectButtonRect, "View Inspector"))
+        {
+            var inspector = InspectorEditorWindow.Create(property.objectReferenceValue, attr.name);
+            inspector.ShowAuxWindow();
+        }      
+        property.serializedObject.ApplyModifiedProperties();
+        EditorGUI.EndProperty();
+    }
+    private GenericMenu GetMenu(SerializedProperty property, System.Type objType)
+    {
+        var menu = new GenericMenu();
+        foreach (var type in ReflectionUtils.ReflectiveEnumerator.GetAllSubclassTypes(objType))
+        {
+            string[] path = type.ToString().Split('.');
+            var name = path[path.Length - 1];
+            menu.AddItem(new GUIContent(name), false, (obj) =>
+            {
+                var newItem = ScriptableObject.CreateInstance((System.Type)obj);
+                newItem.hideFlags = HideFlags.HideInHierarchy;
+                if(property.objectReferenceValue != null)
+                    Object.DestroyImmediate(property.objectReferenceValue, true);
+                AssetDatabase.AddObjectToAsset(newItem, property.serializedObject.targetObject);
+                property.objectReferenceValue = newItem;              
+                property.serializedObject.ApplyModifiedProperties();
+                AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(property.serializedObject.targetObject));
+            }
+            , type);
+        }
+        return menu;
+    }
+}
