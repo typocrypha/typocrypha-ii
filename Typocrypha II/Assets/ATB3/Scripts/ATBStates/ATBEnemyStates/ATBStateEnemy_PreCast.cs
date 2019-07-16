04bb5d4458fe83ab554ca21a9ee277fcc40f86c8
Valentino Abate <valentinoabate1@gmail.com>
2019-07-15 21:17:24 -0700

    Stun 2.0
    - Enemies now give up their cast when stunned in precast
    -  Hacky, should be done better

--- Typocrypha II/Assets/ATB3/Scripts/ATBStates/ATBEnemyStates/ATBStateEnemy_PreCast.cs
+++ Typocrypha II/Assets/ATB3/Scripts/ATBStates/ATBEnemyStates/ATBStateEnemy_PreCast.cs
@@ -9,12 +9,14 @@ namespace ATB3
         // The ID for this specific ATBState
         public override ATBStateID StateID { get { return ATBStateID.PreCast; } }
         private float timer = 0.0f;
+        private ATBEnemy enemyOwner;
 
         // Call upon entering given state
         public override void OnEnter()
         {
+            enemyOwner = Owner as ATBEnemy;
             //Debug.Log("ENEMY " + this.Owner.actorName + " has ENTERED the PRECAST state!");
-            ((ATBEnemy)this.Owner).GetComponent<Animator>().SetTrigger("PreCast");
+            enemyOwner.GetComponent<Animator>().SetTrigger("PreCast");
             timer = 0.0f;
         }
 
@@ -22,6 +24,14 @@ namespace ATB3
         public override void OnUpdate()
         {
             timer += Time.fixedDeltaTime;
+            if(enemyOwner.caster.Stunned)
+            {
+                enemyOwner.caster.Charge = 0;
+                var AI = Owner.GetComponent<CasterAI>();
+                // DEBUG, REMOVE LATER
+                AI.OnAfterCast?.Invoke();
+                Source.PerformTransition(ATBTransition.ToStun);
+            }
             if (timer >= 1.0f && ATBManager.soloStack.Count == 0)
                 Source.PerformTransition(ATBTransition.ToBeforeCast);
             return;
