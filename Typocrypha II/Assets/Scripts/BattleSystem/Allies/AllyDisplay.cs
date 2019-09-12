5510687cfe9b3b44ef01b36eff3f406007e526fe
James Takeshi Iwamasa <jiwamasa@ucsc.edu>
2019-09-11 22:11:54 -0700

    Added basic ally description display
    -Selected ally spell now has description displayed over typocrypha

---
+++ Typocrypha II/Assets/Scripts/BattleSystem/Allies/AllyDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Reference point for displaying ally spell descriptions.
+/// </summary>
+public class AllyDisplay : MonoBehaviour
+{
+    public static AllyDisplay instance;
+    public Text descriptionText;
+
+    private void Awake()
+    {
+        instance = this; // Lazy singleton.
+        gameObject.SetActive(false);
+    }
+}
