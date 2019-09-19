d0767274fa28c07fb3eb8f12cc235d9b5b954a6d
Gigi Bachtel <gbachtel@ucsc.edu>
2019-09-18 22:18:04 -0700

    Added Ally Menu

---
+++ Typocrypha II/Assets/Scripts/BattleSystem/Allies/SetActionMenuPosition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActionMenuPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 position = GetComponent<RectTransform>().anchoredPosition;
+        if (transform.parent.gameObject.GetComponent<RectTransform>().anchoredPosition.x < 0)
+        {
+            position.x = 6.25f;
+
+        }
+        else position.x = -6.25f;
+
+        GetComponent<RectTransform>().anchoredPosition = position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
