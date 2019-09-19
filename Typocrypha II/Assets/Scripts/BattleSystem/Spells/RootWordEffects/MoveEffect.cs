aadec8df8a67ae258a0fe0581d5c9ead27abfecc
Valentino Abate <valentinoabate1@gmail.com>
2019-09-18 18:05:42 -0700

    Movement spell effects and kitsune I-3 moveset
    - Spells can now cause targets to move
    - Kitsune alternates between a fast weaker attack and a movement effect
    - Made running faster
    - Buffed Kitsune's hp

---
+++ Typocrypha II/Assets/Scripts/BattleSystem/Spells/RootWordEffects/MoveEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RandomUtils;
+
+public class MoveEffect : RootWordEffect
+{
+    public bool onlyMoveToUnoccupied;
+    public List<Battlefield.Position> positions;
+    public override CastResults Cast(Caster caster, Caster target, RootCastResults prevResults = null)
+    {
+        CastResults results = new CastResults(caster, target);
+        results.Miss = false;
+        results.DisplayDamage = false;
+        var viablePositions = new List<Battlefield.Position>(positions);
+        viablePositions.RemoveAll((p) => !p.IsLegal || p == target.FieldPos);
+        if(onlyMoveToUnoccupied)
+            viablePositions.RemoveAll((p) => Battlefield.instance.GetObject(p) != null);
+        if (viablePositions.Count > 0)
+        {
+            var pos = RandomU.instance.Choice(viablePositions);
+            Battlefield.instance.Move(target.FieldPos, pos, Battlefield.MoveOption.Swap);
+        }
+        return results;
+    }
+}
---
+++ Typocrypha II/Assets/Scripts/Editor/CustomInspectors/RootWordEffectInspectors/MoveEffectInspector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using SerializableCollections.GUIUtils;
+using GUIUtils;
+
+[CustomEditor(typeof(MoveEffect), true)]
+public class MoveEffectInspector : RootWordEffectInspector
+{
+    public override bool IsChild => true;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        EditorUtils.Separator();
+        var effect = target as MoveEffect;
+        effect.onlyMoveToUnoccupied = EditorGUILayout.ToggleLeft(new GUIContent("Only move to unoccupied"), effect.onlyMoveToUnoccupied);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("positions"), true);
+        serializedObject.ApplyModifiedProperties();
+        if (GUI.changed)
+            EditorUtility.SetDirty(target);
+    }
+}
