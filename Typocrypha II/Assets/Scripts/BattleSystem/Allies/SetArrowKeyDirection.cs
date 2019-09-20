06ad48177a5dde161406525dd5c65936cbd175fd
Gigi Bachtel <gbachtel@ucsc.edu>
2019-09-20 15:22:02 -0700

    Added arrow keys for ally prompts

---
+++ Typocrypha II/Assets/Scripts/BattleSystem/Allies/SetArrowKeyDirection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetArrowKeyDirection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 scale = GetComponent<RectTransform>().localScale;
+        if (transform.parent.parent.gameObject.GetComponent<RectTransform>().anchoredPosition.x < 0)
+        {
+            scale.x = 1f;
+
+        }
+        else scale.x = -1f;
+
+        GetComponent<RectTransform>().localScale = scale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
