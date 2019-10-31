57dfbf100acff09c1eba897712e143c1ab200e02
Charles Miller <Milesferox42@gmail.com>
2019-10-30 20:44:31 -0700

    Tags for Bearded Eel and Puffergeist

--- Typocrypha II/Assets/Scripts/BattleSystem/Caster/Abilities/AbilityChangeTagOnHit.cs
+++ Typocrypha II/Assets/Scripts/BattleSystem/Caster/Abilities/AbilityChangeTagOnHit.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AbilityDoubleDamage : CasterAbility
+public class AbilityChangeTagOnHit : CasterAbility
 {
-    SpellTag tag;
+    public SpellTag onHitTag;
+    public CasterTag removeTag;
+    public CasterTag addTag;
 
     public override void OnCast(Spell spell, RootWordEffect effect, Caster caster, Caster target)
     {
-       if(effect.tags.Contains(tag) && effect is DamageEffect)
-            (effect as DamageEffect).power *= 2;
+
     }
 
     public override void OnHit(Spell spell, RootWordEffect effect, Caster caster, Caster target, CastResults castResults)
     {
+        //if (contains slashing)
+        {
+            target.RemoveTag(removeTag);
+            target.AddTag(addTag);
+        }
         return;
     }
 }
