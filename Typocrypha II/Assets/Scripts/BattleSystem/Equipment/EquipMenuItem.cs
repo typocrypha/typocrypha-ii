eabb0ee47b64a7723dfdc977e863790f01bf7bf6
James Takeshi Iwamasa <james.takeshi.iwamasa@gmail.com>
2020-12-05 16:41:59 -0800

    Started implementing equipment screen

---
+++ Typocrypha II/Assets/Scripts/BattleSystem/Equipment/EquipMenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Single equipped word in equip menu.
+/// </summary>
+public class EquipMenuItem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
