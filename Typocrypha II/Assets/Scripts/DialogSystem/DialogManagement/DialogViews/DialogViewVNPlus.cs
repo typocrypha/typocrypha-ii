6a880424b764e8f559b5680823777e9fd09259ff
Valentino Abate <valentinoabate1@gmail.com>
2021-08-28 15:50:03 -0700

    Separate chat components into their own folder

---
+++ Typocrypha II/Assets/Scripts/DialogSystem/DialogManagement/DialogViews/DialogViewVNPlus.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogViewVNPlus : DialogView
+{
+    public override void CleanUp()
+    {
+
+    }
+
+    public override DialogBox PlayDialog(DialogItem data)
+    {
+        return null;
+    }
+
+    public override void SetEnabled(bool e)
+    {
+        gameObject.SetActive(e);
+    }
+}
