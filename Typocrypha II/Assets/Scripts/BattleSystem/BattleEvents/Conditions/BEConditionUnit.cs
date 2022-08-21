456ad140f2d53bb127af3a18dfaabccaa4809dc0
Valentino Abate <valentinoabate1@gmail.com>
2022-08-20 17:23:56 -0700

    BEConditionUnitStunned + ACT-I-4-4 BE implementation
    - also added the BEConditionUnit base class to streamline conditions that are essentially just a predicate on any/all/specific casters

---
+++ Typocrypha II/Assets/Scripts/BattleSystem/BattleEvents/Conditions/BEConditionUnit.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BEConditionUnit : BattleEventCondition
+{
+    public enum Operator 
+    {
+        Any,
+        All,
+        SpecificName,
+    }
+
+    [SerializeField] Operator op;
+    [SerializeField] private Caster.State casterState = Caster.State.Hostile;
+    [SerializeField] private string specificName = "";
+
+    public override bool Check()
+    {
+        foreach(var caster in Battlefield.instance.Casters)
+        {
+            if (caster.CasterState != casterState)
+                continue;
+            if (op == Operator.SpecificName)
+            {
+                if(caster.DisplayName.ToLower() == specificName.ToLower())
+                {
+                    return CheckCaster(caster);
+                }
+            }
+            else if(op == Operator.Any)
+            {
+                if (CheckCaster(caster))
+                    return true;
+            }
+            else // All operator
+            {
+                if (!CheckCaster(caster))
+                    return false;
+            }
+        }
+        return op == Operator.All;
+    }
+
+    protected abstract bool CheckCaster(Caster caster);
+}
---
+++ Typocrypha II/Assets/Scripts/BattleSystem/BattleEvents/Conditions/BEConditionUnitStunned.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BEConditionUnitStunned : BEConditionUnit
+{
+    protected override bool CheckCaster(Caster caster)
+    {
+        return caster.Stunned;
+    }
+}
