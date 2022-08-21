d3d917e3587db713beeed4003c15811ff97b2685
Gigi Bachtel <35552168+gigsabyte@users.noreply.github.com>
2022-08-20 17:21:36 -0700

    Changed arrow shape and added disabling when message is inactive

--- Typocrypha II/Assets/Scripts/DialogSystem/DialogManagement/DialogViews/VNPlusComponents/VNPlusDialogBoxUI.cs
+++ Typocrypha II/Assets/Scripts/DialogSystem/DialogManagement/DialogViews/VNPlusComponents/VNPlusDialogBoxUI.cs
@@ -10,13 +10,17 @@ public class VNPlusDialogBoxUI : MonoBehaviour
     [SerializeField] private Color dimColor = new Color(dimAmount, dimAmount, dimAmount, 1);
     [SerializeField] private Image iconImage;
     [SerializeField] private Image[] borderImages;
+    [SerializeField] private Image arrowImage;
     [SerializeField] private CanvasGroup canvasGroup;
+
     public void Bind(CharacterData character)
     {
         foreach(var image in borderImages)
         {
             image.color = character.characterColorLight;
         }
+        arrowImage.enabled = true;
+        arrowImage.color = character.characterColorLight;
         iconImage.sprite = character.chat_icon;
         iconImage.color = Color.white;
         canvasGroup.alpha = 1;
@@ -31,5 +35,6 @@ public class VNPlusDialogBoxUI : MonoBehaviour
             var image = borderImages[i];
             tweenInfo.Start(image.DOColor(image.color * dimColor, tweenInfo.Time), false);
         }
+        arrowImage.enabled = false;
     }
 }
