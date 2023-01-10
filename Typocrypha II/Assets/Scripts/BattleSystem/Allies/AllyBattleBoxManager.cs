4eba63070cb3977fe7fd54cd7c8047bd90bf85c0
Valentino Abate <valentinoabate1@gmail.com>
2023-01-10 00:10:53 -0800

    Ally battle box manager
    - isn't currently hooked up to bubble view control nodes
    - doesn't currently set itself at the beginning of the battle
    - Supports adding new characters, which will swap characters if one is already active
    - Supports showing / hiding the current character
    - tested with debugging code that I have since removed
    - Setting the expression, etc can be done from he publicly accessible VNPlusCharacter

---
+++ Typocrypha II/Assets/Scripts/BattleSystem/Allies/AllyBattleBoxManager.cs
@@ -0,0 +1,92 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Note, currently doesn't support two columns, but could in the future
+public class AllyBattleBoxManager : MonoBehaviour
+{
+    public static AllyBattleBoxManager instance;
+    [SerializeField] private VNPlusCharacter[] rightColumnCharas;
+    [SerializeField] private RectTransform onScreenPos;
+    [SerializeField] private RectTransform offScreenPos;
+    [SerializeField] private TweenInfo joinTweenInfo;
+
+    public VNPlusCharacter CurrentChar => rightColumnCharas[rightColumnIndex];
+    private int rightColumnIndex = 0;
+    private bool allCharactersHidden = false;
+
+    // Toggle between 0 and 1
+    private static int Toggle(int arg) => Mathf.Abs(arg - 1);
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    public void SetCharacterInstant(CharacterData data)
+    {
+        CurrentChar.Data = data;
+        CurrentChar.NameText = data.mainAlias;
+    }
+
+    public YieldInstruction AddCharacter(CharacterData data)
+    {
+        if (allCharactersHidden)
+        {
+            SetCharacterInstant(data);
+            return ShowCharacter();
+        }
+        var oldChar = CurrentChar;
+        // Set to ne character
+        rightColumnIndex = Toggle(rightColumnIndex);
+        SetCharacterInstant(data);
+        // Set new Character to be layered above old one
+        CurrentChar.transform.SetAsLastSibling();
+        // Remove old character
+        PlayJoinLeaveTween(oldChar, offScreenPos, true);
+        // Join New Character
+        PlayJoinLeaveTween(CurrentChar, onScreenPos, false);
+        return joinTweenInfo.WaitForCompletion();
+    }
+
+    public YieldInstruction HideCharacter()
+    {
+        if (allCharactersHidden)
+        {
+            return null;
+        }
+        // Move current character offscreen
+        PlayJoinLeaveTween(CurrentChar, offScreenPos, true);
+        // Reset values
+        rightColumnIndex = 0;
+        allCharactersHidden = true;
+        return joinTweenInfo.WaitForCompletion();
+    }
+
+    public YieldInstruction ShowCharacter()
+    {
+        if (!allCharactersHidden)
+        {
+            return null;
+        }
+        // Move current character offscreen
+        PlayJoinLeaveTween(CurrentChar, onScreenPos, true);
+        allCharactersHidden = false;
+        return joinTweenInfo.WaitForCompletion();
+    }
+
+    private void PlayJoinLeaveTween(VNPlusCharacter chara, RectTransform target, bool complete)
+    {
+        joinTweenInfo.Start(chara.MainRect.DOAnchorPosX(target.anchoredPosition.x, joinTweenInfo.Time), complete);
+    }
+
+}
