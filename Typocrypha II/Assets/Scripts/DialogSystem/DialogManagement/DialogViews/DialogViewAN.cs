cdb63f76ec7923bcd9a9a792e25cbc8c90fbf47c
Valentino Abate <valentinoabate1@gmail.com>
2023-01-28 17:56:46 -0800

    AN Dialog View 1
    - basic view transition and sizing fixes
    - update background to an image
    - add testing scene
    - doesn't currently display messages

--- Typocrypha II/Assets/Scripts/DialogSystem/DialogManagement/DialogViews/DialogViewAN.cs
+++ Typocrypha II/Assets/Scripts/DialogSystem/DialogManagement/DialogViews/DialogViewAN.cs
@@ -1,19 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 /// <summary>
 /// Audio novel style dialog. i.e. Floating text over full background.
 /// </summary>
 public class DialogViewAN : DialogView
 {
-    public RectTransform ANContent; // Content of scroll view
+    [SerializeField] private RectTransform ANContent; // Content of scroll view
+    [SerializeField] private Image background;
+
+    private void Awake()
+    {
+        background.color = Color.clear;    
+    }
 
     public override DialogBox PlayDialog(DialogItem data)
     {
         if (!IsDialogItemCorrectType(data, out DialogItemAN dialogItem))
-            return null;
-        
+            return null;       
         var dialogBox = Instantiate(dialogBoxPrefab, ANContent).GetComponent<DialogBox>();
         dialogBox.SetupAndStartDialogBox(dialogItem);
         return dialogBox;
@@ -22,6 +29,22 @@ public class DialogViewAN : DialogView
     public override void SetEnabled(bool e)
     {
         gameObject.SetActive(e);
+        if (!e)
+        {
+            background.color = Color.clear;
+        }
+    }
+
+    public override IEnumerator PlayEnterAnimation()
+    {
+        var fadeIn = background.DOFade(1, 2);
+        yield return fadeIn.WaitForCompletion();
+    }
+
+    public override IEnumerator PlayExitAnimation(bool isEndOfDialog)
+    {
+        var fadeOut = background.DOFade(0, 2);
+        yield return fadeOut.WaitForCompletion();
     }
 
     // Clear all AN dialogue (TODO)
