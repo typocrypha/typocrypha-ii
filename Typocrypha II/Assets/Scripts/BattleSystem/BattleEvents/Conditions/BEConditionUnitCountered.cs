e892d8d25b5b85750de45129afb3a6baa43b80d2
Valentino Abate <valentinoabate1@gmail.com>
2023-02-11 15:46:43 -0800

    Add unit countered BECondition
    - also update condition for ACT-I-4-4-Stun
    - that event isn't actually in the scene

---
+++ Typocrypha II/Assets/Scripts/BattleSystem/BattleEvents/Conditions/BEConditionUnitCountered.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BEConditionUnitCountered : BEConditionUnit
+{
+    protected override bool CheckCaster(Caster caster)
+    {
+        return caster.Countered;
+    }
+}
