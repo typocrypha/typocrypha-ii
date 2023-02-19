dfc1415cf916239ba1d6414dec038d638b9117b2
Valentino Abate <valentinoabate1@gmail.com>
2023-02-18 18:08:51 -0800

    Placeholder BIs for bunbuku and ogama defeated
    - they don't trigger scenes yet, they just end the wave

---
+++ Typocrypha II/Assets/Scripts/BattleSystem/BattleEvents/Conditions/BEConditionUnitBattleStatus.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BEConditionUnitBattleStatus : BEConditionUnit
+{
+    [SerializeField] private List<Caster.BattleStatus> statuses = new List<Caster.BattleStatus>();
+    protected override bool CheckCaster(Caster caster)
+    {
+        return statuses.Contains(caster.BStatus);
+    }
+}
