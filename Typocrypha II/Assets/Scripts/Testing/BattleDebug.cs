608c5ba304bc035abee67bc4756a4480588951f1
Valentino Abate <valentinoabate1@gmail.com>
2023-03-05 18:15:37 -0800

    Add skip battle interrupt battle debug function
    using f1 in a battle interrupt will skip it

---
+++ Typocrypha II/Assets/Scripts/Testing/BattleDebug.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleDebug : MonoBehaviour
+{
+#if !DEBUG
+    private void Awake()
+    {
+        Destroy(gameObject);
+    }
+#else
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1) && !DialogManager.instance.PH.Pause)
+        {
+            DialogManager.instance.StopAllCoroutines();
+            DialogManager.instance.Hide(true, DialogManager.instance.CleanUp);
+        }
+    }
+#endif
+}
