1892fd11a65ff02edc6dac2a8ff4617de38d332c
Valentino Abate <valentinoabate1@gmail.com>
2023-04-02 19:09:50 -0700

    Remove bad pausable extensions
    - doesn't add anything optimization-wise and makes it harder to track usages

--- Typocrypha II/Assets/Scripts/Utility/Pausing/PausableExtensions.cs
+++ Typocrypha II/Assets/Scripts/Utility/Pausing/PausableExtensions.cs
@@ -1,6 +1,4 @@
 ﻿public static class PausableExtensions
 {
     public static bool IsPaused(this IPausable self) => self.PH.Pause;
-    public static void Pause(this IPausable self) => self.PH.Pause = true;
-    public static void Unpause(this IPausable self) => self.PH.Pause = false;
 }
