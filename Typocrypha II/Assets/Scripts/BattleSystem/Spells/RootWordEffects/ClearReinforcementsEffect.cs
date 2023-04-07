b23c756c11cc070104d4e7ccc3205a8625d1b991
Valentino Abate <valentinoabate1@gmail.com>
2023-04-07 16:42:51 -0700

    Added End debug spell
    - essentially clears the reinforcements and the does a perish
    - useful for ending horde encounters early for testing purposes

---
+++ Typocrypha II/Assets/Scripts/BattleSystem/Spells/RootWordEffects/ClearReinforcementsEffect.cs
@@ -0,0 +1,9 @@
+﻿public class ClearReinforcementsEffect : RootWordEffect
+{
+    public override bool CanCrit => false;
+    public override CastResults Cast(Caster caster, Caster target, RootCastData spellData, Damage.SpecialModifier mod, RootCastResults prevResults = null)
+    {
+        BattleManager.instance.ClearReinforcements();
+        return new CastResults(caster, target) { DisplayDamage = false, };
+    }
+}
