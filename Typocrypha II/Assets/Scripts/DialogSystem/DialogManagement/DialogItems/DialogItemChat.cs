3e19f8867f12d12d1565815839b550d70e53ba4c
Valentino Abate <valentinoabate1@gmail.com>
2023-09-02 16:45:08 -0700

    Chat view timestamp expressions (basic)
    - no fading animation

--- Typocrypha II/Assets/Scripts/DialogSystem/DialogManagement/DialogItems/DialogItemChat.cs
+++ Typocrypha II/Assets/Scripts/DialogSystem/DialogManagement/DialogItems/DialogItemChat.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class DialogItemChat : DialogItemMessage
 {
     public IconSide iconSide;
-    public DialogItemChat(string text, List<AudioClip> voice, List<CharacterData> characterData, string[] characterNames, IconSide iconSide) 
+    public string timeText;
+    public DialogItemChat(string text, List<AudioClip> voice, List<CharacterData> characterData, string[] characterNames, IconSide iconSide, string timeText) 
         : base(text, voice, characterData, characterNames)
     {
         this.iconSide = iconSide;
+        this.timeText = timeText;
     }
 
     public override System.Type GetView()
---
+++ Typocrypha II/Assets/Scripts/DialogSystem/DialogManagement/DialogViews/ChatComponents/ChatDialogBoxUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ChatDialogBoxUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timeText;
+
+    public void Bind(DialogItemChat dialogItem)
+    {
+        timeText.text = dialogItem.timeText;
+    }
+}
