eff1c8bffdc338f188b0f40810712625eae20463
Valentino Abate <valentinoabate1@gmail.com>
2024-07-21 19:02:52 -0700

    Add Save Data Tools
    - lets you wipe your save files

---
+++ Typocrypha II/Assets/Scripts/Editor/SaveDataTools.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public static class SaveDataTools
+{
+    [MenuItem("Tools/Save Data/Reset Campaign Save Data")]
+    public static void ResetCampaignSaveData()
+    {
+        FileUtil.DeleteFileOrDirectory(SaveManager.SaveFilePath(0));
+        FileUtil.DeleteFileOrDirectory(SaveManager.SaveFilePath(SaveManager.debugSaveFile));
+    }
+
+    [MenuItem("Tools/Save Data/Reset Global Save Data")]
+    public static void ResetGlobalSaveData()
+    {
+        FileUtil.DeleteFileOrDirectory(SaveManager.GlobalSaveFilePath());
+    }
+
+    [MenuItem("Tools/Save Data/Reset All Save Data")]
+    public static void ResetAllSaveData()
+    {
+        ResetCampaignSaveData();
+        ResetGlobalSaveData();
+    }
+}
