0dd28723f9d6216e20eb4ce3cb58f6c69e48a39f
Valentino Abate <valentinoabate1@gmail.com>
2024-08-10 17:13:06 -0700

    Buff Bulwark
    - significantly more powerful (may be too powerful)

--- Typocrypha II/Assets/Scripts/BattleSystem/EquipmentWords/BadgeEffectCritBlock.cs
+++ Typocrypha II/Assets/Scripts/BattleSystem/EquipmentWords/BadgeEffectCritBlock.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class BadgeEffectCritBlock : BadgeEffect
 {
-    public const float baseCritChance = 0.075f;
+    public const float baseCritChance = 0.20f;
+
+    private int missCounter = 0;
+
     public override void Equip(Player player)
     {
         player.AddActiveAbilities(Caster.ActiveAbilities.CriticalBlock);
@@ -15,8 +18,18 @@ public class BadgeEffectCritBlock : BadgeEffect
         player.RemoveActiveAbilities(Caster.ActiveAbilities.CriticalBlock);
     }
 
-    public static bool RollForCritical(Caster player)
+    public bool RollForCritical(Caster player)
     {
-        return UnityEngine.Random.Range(0, 1f) <= Mathf.Max(0, baseCritChance + (player.Stats.Luck * 0.015f));
+        float missComponent = missCounter * 0.1f + (player.Stats.Luck * 0.01f);
+        if (UnityEngine.Random.Range(0, 1f) <= Mathf.Max(0, baseCritChance + missComponent + (player.Stats.Luck * 0.025f)))
+        {
+            missCounter = 0;
+            return true;
+        }
+        else
+        {
+            missCounter++;
+            return false;
+        }
     }
 }
