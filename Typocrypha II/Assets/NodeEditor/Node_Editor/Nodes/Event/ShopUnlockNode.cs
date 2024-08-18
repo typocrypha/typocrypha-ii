4d8cf825438eaad5734d3f40e4984042c2c16e00
Valentino Abate <valentinoabate1@gmail.com>
2024-08-17 17:55:38 -0700

    Shop Unlock Node Functionality
    - node that unlocks badges in the shop

---
+++ Typocrypha II/Assets/NodeEditor/Node_Editor/Nodes/Event/ShopUnlockNode.cs
@@ -0,0 +1,32 @@
+﻿using NodeEditorFramework;
+using NodeEditorFramework.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameflow
+{
+    [Node(false, "Event/Shop Unlock", typeof(BattleCanvas), typeof(DialogCanvas))]
+    public class ShopUnlockNode : BaseNodeIO
+    {
+        private const string id = "ShopUnlockNode";
+        public override string GetID => id;
+
+        public override string Title => "Shop Unlock";
+        public override Vector2 MinSize => new Vector2(150, 40);
+
+        public BadgeWord.ShopUnlockCodes unlockCodes;
+
+        public override void NodeGUI()
+        {
+            GUILayout.Space(4);
+            GUILayout.BeginVertical("Box");
+
+            unlockCodes = (BadgeWord.ShopUnlockCodes)RTEditorGUI.EnumFlagsPopup(unlockCodes);
+
+            //Don't know why this code needs to be here exactly, but it makes everything nicer? maybe add to some static stuff?
+            GUILayout.BeginHorizontal();
+            GUILayout.EndHorizontal();
+        }
+    }
+}
