b4ae5560f28af848b4f2f2b6ee16498085454ff1
Valentino Abate <valentinoabate1@gmail.com>
2024-09-14 17:59:05 -0700

    FOCUS Badge

---
+++ Typocrypha II/Assets/Scripts/BattleSystem/EquipmentWords/BadgeEffectExtendPromptTimer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadgeEffectExtendPromptTimer : BadgeEffect
+{
+    [SerializeField] private float multiplier;
+    public float Multiplier => multiplier;
+}
