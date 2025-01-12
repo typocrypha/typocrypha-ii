574001feab55895709428d7fb20bc4dbecaa8f51
Gigi Bachtel <35552168+gigsabyte@users.noreply.github.com>
2025-01-11 16:38:20 -0800

    Shop menu updates

---
+++ Typocrypha II/Assets/Scripts/Graphics/Background/TiledBackgroundScroller.cs
@@ -0,0 +1,27 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TiledBackgroundScroller : MonoBehaviour
+{
+    [SerializeField]
+    private Image image;
+
+    [SerializeField]
+    private float scrollSpeed = 1.0f;
+
+    private RectTransform imageTransform;
+
+    private void Start()
+    {
+        imageTransform = (RectTransform)image.transform;
+
+        var target = new Vector2(imageTransform.anchoredPosition.x + image.sprite.rect.width,
+                                   imageTransform.anchoredPosition.y + image.sprite.rect.height);
+        imageTransform.DOAnchorPos(target, scrollSpeed)
+            .SetEase(Ease.Linear)
+            .SetLoops(-1, LoopType.Restart);
+    }
+}
