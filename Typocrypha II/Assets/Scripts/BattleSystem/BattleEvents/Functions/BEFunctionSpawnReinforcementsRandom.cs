485ab0eb009a4a7f10b626f0ede98fd963a60d54
Valentino Abate <valentinoabate1@gmail.com>
2025-02-22 17:07:09 -0800

    Mari Phase 1 Revamp 2
    - many design tweaks

--- Typocrypha II/Assets/Scripts/BattleSystem/BattleEvents/Functions/BEFunctionSpawnReinforcementsRandom.cs
+++ Typocrypha II/Assets/Scripts/BattleSystem/BattleEvents/Functions/BEFunctionSpawnReinforcementsRandom.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 
 public class BEFunctionSpawnReinforcementsRandom : BEFunctionSpawnReinforcementsAbstract
 {
+    [SerializeField] private bool preventRepeats = false;
+
+    private int lastIndex = -1;
     protected override bool Consume => false;
 
     protected override int GetReinforcementIndex(List<GameObject> reinforcements)
     {
-        return RandomUtils.RandomU.instance.RandomInt(0, reinforcements.Count);
+        if (!preventRepeats || lastIndex == -1 || reinforcements.Count == 1)
+        {
+            return lastIndex = RandomUtils.RandomU.instance.RandomInt(0, reinforcements.Count);
+        }
+        var options = new List<int>(reinforcements.Count);
+        for (int i = 0; i < reinforcements.Count; i++)
+        {
+            if (i == lastIndex)
+                continue;
+            options.Add(i);
+        }
+        return lastIndex = RandomUtils.RandomU.instance.Choice(options);
     }
 }
