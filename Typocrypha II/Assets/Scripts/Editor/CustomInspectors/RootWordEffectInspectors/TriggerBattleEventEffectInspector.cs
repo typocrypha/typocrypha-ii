76b75b37079b490db2e2b4ff6e4bde060e796556
Valentino Abate <valentinoabate1@gmail.com>
2025-03-01 16:09:29 -0800

    Inspector

---
+++ Typocrypha II/Assets/Scripts/Editor/CustomInspectors/RootWordEffectInspectors/TriggerBattleEventEffectInspector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(TriggerBattleEventEffect), true)]
+public class TriggerBattleEventEffectInspector : RootWordEffectInspector
+{
+    public override bool IsChild => true;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        EditorUtils.Separator();
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("id"));
+        serializedObject.ApplyModifiedProperties();
+        if (GUI.changed)
+            EditorUtility.SetDirty(target);
+    }
+}
