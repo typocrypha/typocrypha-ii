c8c4897f35daa8814c724947770281edbb3f705c
Valentino Abate <valentinoabate1@gmail.com>
2025-06-19 16:05:34 -0700

    Add animation effect
    - a root effect that has no function other than playing a spell effect

---
+++ Typocrypha II/Assets/Scripts/BattleSystem/Spells/RootWordEffects/AnimationEffect.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEffect : RootWordEffect
+{
+    public override CastResults Cast(Caster caster, Caster target, RootCastData spellData, Damage.DamageModifier mod, RootCastResults prevResults = null)
+    {
+        return InitializeCastResults(caster, target, mod);
+    }
+}
