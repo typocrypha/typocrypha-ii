ca43cea218d86d819578f9497ff2eb90281b7952
Justin Law <jlawhome08@gmail.com>
2025-07-03 22:01:13 -0700

    Create CSV Parser for Importing TIPS Entries
    Currently only reads the csv file and writes the eventual asset path to console

---
+++ Typocrypha II/Assets/NodeEditor/Editor/TIPSBundleParserWindow.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NodeEditorFramework;
+using System.IO;
+
+public class TIPSBundleParserWindow : EditorWindow
+{
+    public TextAsset csvFile;
+
+    [MenuItem("Window/TIPSBundleParser")]
+    public static void ShowWindow()
+    {
+        GetWindow(typeof(TIPSBundleParserWindow), false, "TIPSBundleParser");
+    }
+
+    void OnGUI()
+    {
+        // Main asset fields
+        GUILayout.BeginVertical("Box");
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("TIPS Entries CSV");
+        csvFile = EditorGUILayout.ObjectField(csvFile, typeof(TextAsset), false) as TextAsset;
+        GUILayout.EndHorizontal();
+
+        GUILayout.EndVertical();
+
+        string assetPath = AssetDatabase.GetAssetPath(csvFile.GetInstanceID());
+        if (assetPath != null && GUILayout.Button("Build TIPS", GUILayout.Width(120f)))
+        {
+            const string assetPathPrefix = "Assets";
+            var fullPath = Application.dataPath + assetPath.Substring(assetPathPrefix.Length);
+            TIPSBundleParser.Parse(fullPath);
+        }
+    }
+}
