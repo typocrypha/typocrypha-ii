ce3d922dafeff59bdf1ba90b58f05bbe095d9cb4
Valentino Abate <valentinoabate1@gmail.com>
2025-07-12 16:44:13 -0700

    Revert "Serializable Dictionary Init Override"
    This reverts commit c1b1e5c2400721f0961852ae3e53e20c362c1fba.

--- Typocrypha II/Assets/Scripts/Utility/BetterBundles/TIPSBundle.cs
+++ Typocrypha II/Assets/Scripts/Utility/BetterBundles/TIPSBundle.cs
@@ -6,13 +6,5 @@ using UnityEngine;
 public class TIPSBundle : BetterBundle
 {
     public TIPSDictionary entries;
-
-    [System.Serializable] 
-    public class TIPSDictionary : SerializableDictionary<string, TIPSEntryData> 
-    {
-        protected override void InitializeDictionary()
-        {
-            _dictionary = new Dictionary<string, TIPSEntryData>(System.StringComparer.InvariantCultureIgnoreCase);
-        }
-    }
+    [System.Serializable] public class TIPSDictionary : SerializableDictionary<string, TIPSEntryData> { };
 }
