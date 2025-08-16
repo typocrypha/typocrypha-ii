8fafaa1f7857519170716732f724d8f5fd5e09d9
Justin Law <jlawhome08@gmail.com>
2025-08-16 16:06:13 -0700

    Add Text Effect Parsing for TIPS Entry Panel

--- Typocrypha II/Assets/Scripts/TIPS/TIPSEntryPanel.cs
+++ Typocrypha II/Assets/Scripts/TIPS/TIPSEntryPanel.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
@@ -8,12 +7,24 @@ public class TIPSEntryPanel : MonoBehaviour
     [SerializeField] TextMeshProUGUI title;
     [SerializeField] TextMeshProUGUI content;
 
+    private readonly List<MonoBehaviour> dummy = new List<MonoBehaviour>();
+
     public void SetTitle(string text)
     {
         title.text = text;
     }
     public void SetContent(string text)
     {
-        content.text = text;
+        FXText.TMProEffect.Cleanup(dummy);
+        content.text = DialogParser.instance.Parse
+            (
+                line: text,
+                fxContainer: gameObject,
+                textUI:content,
+                textEvents:null,
+                textEffects: dummy,
+                tipsEntries: out var _,
+                createEvents:false
+            );
     }
 }
