93694b69aba2543694ec73000abb027a3043ac77
Valentino Abate <valentinoabate1@gmail.com>
2025-11-16 21:48:32 -0800

    Switch target to crit instead of auto-weak

--- Typocrypha II/Assets/Scripts/BattleSystem/StatusEffects/StatusTargeted.cs
+++ Typocrypha II/Assets/Scripts/BattleSystem/StatusEffects/StatusTargeted.cs
@@ -1,26 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StatusTargeted : StatusRemoveAfterHitOrCast
 {
-    public CasterTagDictionary.ReactionMultiSet GetReactions(SpellTag tag)
-    {
-        return new CasterTagDictionary.ReactionMultiSet()
-        {
-            Reaction.Weak,
-        };
-    }
-
     protected override void Initialize()
     {
         base.Initialize();
-        affected.ExtraReactions += GetReactions;
+        affected.OnBeforeHitResolved += ApplyStatus;
     }
 
     public override void Cleanup()
     {
         base.Cleanup();
-        affected.ExtraReactions -= GetReactions;
+        affected.OnBeforeHitResolved -= ApplyStatus;
+    }
+
+    private void ApplyStatus(RootWordEffect effect, Caster caster, Caster target, RootCastData spellData, CastResults data)
+    {
+        if (data.Mod == Damage.SpecialModifier.CritBlock || data.Effectiveness == Reaction.Block)
+            return;
+        Damage.StandardSpecialMod(data, effect, caster, target, Damage.SpecialModifier.Critical);
     }
 }
