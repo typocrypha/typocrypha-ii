9d59ab41958036f8e782b3d81ad19b0a5baff405
Justin Law <jlawhome08@gmail.com>
2026-01-03 17:33:00 -0800

    Control Guides, New TIPS Controls

---
+++ Typocrypha II/Assets/Scripts/DialogSystem/DialogManagement/DialogViews/ControlGuide.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ControlGuide : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI[] controlTexts;
+
+    public void SetControl(int index, string text)
+    {
+        controlTexts[index].text = text;
+    }
+}
