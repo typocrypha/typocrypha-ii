f79272ab2a83d9aba71d6788d284133a48d4f400
Valentino Abate <valentinoabate1@gmail.com>
2026-02-07 17:09:12 -0800

    Mari Rebalances
    - make storm orbs slightly slower when keyboard has key effects
    - remove ice orb weaknesses resistances
    - make phase 1 and 2 slightly shorter

---
+++ Typocrypha II/Assets/Scripts/BattleSystem/Caster/AI/AIComponents/AISlowdownWithKeyEffects.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISlowdownWithKeyEffects : AIComponent
+{
+    private int speedMod = 0;
+
+    protected override void AddListeners()
+    {
+        base.AddListeners();
+        caster.OnSpellChanged += OnSpellChanged;
+    }
+
+    protected override void RemoveListeners()
+    {
+        base.RemoveListeners();
+        caster.OnSpellChanged -= OnSpellChanged;
+    }
+
+    private void OnSpellChanged(Caster arg1, Spell arg2)
+    {
+        // Undo previous stat change
+        caster.Stats.Spd += speedMod;
+        // Calculate speed mod
+        var keyEffects = new HashSet<Typocrypha.KeyEffect.EffectType>();
+        foreach(var keyEffect in Typocrypha.Keyboard.instance.allEffects)
+        {
+            if (!keyEffects.Contains(keyEffect.Value.EffectID))
+            {
+                keyEffects.Add(keyEffect.Value.EffectID);
+            }
+        }
+        speedMod = keyEffects.Count;
+        caster.Stats.Spd -= speedMod;
+    }
+}
