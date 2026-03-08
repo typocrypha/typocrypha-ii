6fdbe23630fd8e9dcd6b744573a91cdd24406657
Valentino Abate <valentinoabate1@gmail.com>
2026-03-07 16:52:32 -0800

    Move scouter to tilde

--- Typocrypha II/Assets/Scripts/BattleSystem/Scouter/Scouter.cs
+++ Typocrypha II/Assets/Scripts/BattleSystem/Scouter/Scouter.cs
@@ -111,7 +111,7 @@ public class Scouter : MonoBehaviour, IPausable
 
     private bool ScouterKeyPressed()
     {
-        return Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl);
+        return Input.GetKeyDown(KeyCode.Tilde) || Input.GetKeyDown(KeyCode.Tilde);
     }
 
     private void ToggleScouter()
